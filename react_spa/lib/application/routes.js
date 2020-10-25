8b47be71710a43a4fb107ba950d03a132f25dff8
Alexey D <ord.alwo@gmail.com>
2020-10-25 12:57:54 +0300

    Add routes

---
+++ react_spa/lib/application/routes.js
@@ -0,0 +1,6 @@
+import { composeRoutes } from "broutes";
+
+export default composeRoutes(r => {
+  r.route('/', { name: 'root' });
+  r.route('/hello_world');
+});
--- react_spa/lib/application/routes_switch.jsx
+++ react_spa/lib/application/routes_switch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import routes from "application/routes";
 import { Switch, Route } from "react-router-dom";
 import HomePage from 'home_page';
 import HelloWorldPage from "hello_world/hello_world_page";
@@ -7,8 +8,8 @@ import NotFoundPage from "error_pages/not_found_page";
 export default function RoutesSwitch() {
   return (
     <Switch>
-      <Route exact path='/'><HomePage/></Route>
-      <Route exact path='/hello_world'><HelloWorldPage/></Route>
+      <Route exact path={routes.rootPath.raw}><HomePage/></Route>
+      <Route exact path={routes.helloWorldPath.raw}><HelloWorldPage/></Route>
       <Route path='*'><NotFoundPage/></Route>
     </Switch>
   );
