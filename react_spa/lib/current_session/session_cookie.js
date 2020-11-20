381635b690dbbfc7ce92afd8e5c144bf3bb5eaf6
Alexey D <ord.alwo@gmail.com>
2020-11-20 15:12:14 +0300

    SessionCookie

---
+++ react_spa/lib/current_session/session_cookie.js
@@ -0,0 +1,33 @@
+import Cookies from 'js-cookie';
+import { deepCamelizeKeys, isPlainObject } from "utils/object";
+import { safeParseJSON } from "utils/json";
+
+const COOKIE_NAME = 'current_session';
+
+export default {
+  COOKIE_NAME,
+
+  extract() {
+    let cookie = Cookies.get(COOKIE_NAME);
+
+    if (cookie) {
+      return parseCookieJSON(cookie);
+    } else {
+      return null;
+    }
+  },
+
+  set(value) {
+    Cookies.set(COOKIE_NAME, JSON.stringify(value));
+  }
+};
+
+function parseCookieJSON(cookie) {
+  let parsed = safeParseJSON(cookie);
+
+  if (parsed.success && isPlainObject(parsed.value)) {
+    return deepCamelizeKeys(parsed.value);
+  } else {
+    return null;
+  }
+}
---
+++ react_spa/test/lib/current_session/session_cookie_test.js
@@ -0,0 +1,38 @@
+import jutest from "jutest";
+import Cookies from 'js-cookie';
+import SessionCookie from "current_session/session_cookie";
+
+jutest("SessionCookie", s => {
+  s.describe(".extract()", s => {
+    s.test("returns null if cookie is not set", t => {
+      t.equal(SessionCookie.extract(), null);
+    });
+
+    s.test("parses cookie value", t => {
+      Cookies.set(SessionCookie.COOKIE_NAME, JSON.stringify({ foo: 'bar' }));
+      t.same(SessionCookie.extract(), { foo: 'bar' });
+    });
+
+    s.test("returns null if cookie value cant be parsed", t => {
+      Cookies.set(SessionCookie.COOKIE_NAME, 'foobar');
+      t.same(SessionCookie.extract(), null);
+    });
+
+    s.test("it camelizes parsed cookie's keys", t => {
+      Cookies.set(SessionCookie.COOKIE_NAME, JSON.stringify({ foo_bar: 'bar' }));
+      t.same(SessionCookie.extract(), { fooBar: 'bar' });
+    });
+
+    s.test("it returns null if cookie value is not an object", t => {
+      Cookies.set(SessionCookie.COOKIE_NAME, JSON.stringify(1));
+      t.same(SessionCookie.extract(), null);
+    });
+  });
+
+  s.describe(".set()", s => {
+    s.test("stringifies and updates session cookie", t => {
+      SessionCookie.set({a: 1});
+      t.same(SessionCookie.extract(), {a: 1});
+    });
+  });
+});
