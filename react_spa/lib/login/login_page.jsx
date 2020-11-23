6c04e67e2299296f0767f1ecb833c6969ac9bb70
Alexey D <ord.alwo@gmail.com>
2020-11-23 14:57:42 +0300

    Login page styling

--- react_spa/lib/login/login_page.jsx
+++ react_spa/lib/login/login_page.jsx
@@ -17,13 +17,48 @@ export default function LoginPage() {
     return <Redirect to={routes.rootPath()}/>;
   } else {
     return (
-      <div test-id="login-page">
-        <form onSubmit={handleSubmit(onSubmit)} test-id='login-form'>
-          <input name="email" ref={register}/>
-          <input name="password" ref={register}/>
-          <input type="submit"/>
-        </form>
-      </div>
+      <section className="hero has-background-black-ter is-fullheight guest-hero" test-id="login-page">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-two-fifths">
+                <div className="box login-box">
+                  <div className="has-text-centered mb-4">
+                    <h2 className="title">Rails-React Template</h2>
+                  </div>
+                  <form onSubmit={handleSubmit(onSubmit)} test-id='login-form'>
+                    <div className="field">
+                      <div className="control has-icons-right">
+                        <input className="input" type="email" autoFocus placeholder="user@example.com" name="email" ref={register}/>
+                        <span className="icon is-small is-right"><i className="fas fa-user"></i></span>
+                      </div>
+                    </div>
+
+                    <div className="field">
+                      <div className="control has-icons-right">
+                        <input className="input" type="password" placeholder="********" name="password" ref={register}/>
+                        <span className="icon is-small is-right"><i className="fas fa-lock"></i></span>
+                      </div>
+                    </div>
+
+                    <div className="field is-flex is-justify-content-space-between">
+                      <div><a>Forgot password?</a></div>
+                      <div className="control">
+                        <button type="submit" className="button is-info">
+                          <span className="icon is-small">
+                            <i className="fas fa-key"></i>
+                          </span>
+                          <span>Log In</span>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
